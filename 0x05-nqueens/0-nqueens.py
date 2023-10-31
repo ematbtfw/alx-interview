465865e297fd8e69ea9f21aee303e413dc31419b
ematbtfw <105008544+ematbtfw@users.noreply.github.com>
2023-10-31 11:45:33 +0300

    task0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,22 +1,54 @@
-def place_queen(n, row, solutions, cols, upright, downright):
-    for col in range(n):
-        if col not in cols and row-col not in upright and row+col not in downright:
-            solutions.append([[row, col]] + sol for sol in solutions)
-            place_queen(n, row+1, solutions, cols | {col}, upright | {row-col}, downright | {row+col})
-            solutions = []
-            cols = cols - {col}
-            upright = upright - {row-col}
-            downright = downright - {row+col}
-            break
+#!/usr/bin/python3
+"""Solving N Queens with Backtracing"""
+import sys
 
-def solve_n_queens(n):
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit() or int(sys.argv[1]) < 4:
+
+def nqueens(n, y, board):
+    """
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form
+    """
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
+
+
+def main():
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-    solutions = []
-    place_queen(n, 0, solutions, set(), set(), set())
-    return solutions
+    nqueens(n, 0, [])
 
-for solution in solve_n_queens(int(sys.argv[1])):
-    print(solution)
+if __name__ == '__main__':
+    main()
