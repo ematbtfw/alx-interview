7869ec650cc792048a91029706668fe07f66fa2e
ematbtfw <105008544+ematbtfw@users.noreply.github.com>
2023-11-11 16:00:22 +0300

    task0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,24 @@
 #!/usr/bin/node
-
-const request = require('request');
+const axios = require('axios');
 
 const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
-    }
-  }));
-};
+axios.get(apiUrl)
+  .then(response => {
+    const film = response.data;
+    const characters = film.characters;
 
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
-  }
-};
+    const characterPromises = characters.map(characterUrl =>
+      axios.get(characterUrl)
+        .then(charResponse => charResponse.data.name)
+    );
 
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
-    }
-  }
-};
-
-getCharNames();
+    return Promise.all(characterPromises);
+  })
+  .then(characterNames => {
+    characterNames.forEach(name => console.log(name));
+  })
+  .catch(error => {
+    console.error(error.message);
+  });
