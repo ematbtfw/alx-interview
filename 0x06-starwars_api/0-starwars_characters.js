4c6c2e368b4eacb66ee99487b6ff04f45d8587cb
ematbtfw <105008544+ematbtfw@users.noreply.github.com>
2023-11-11 16:04:34 +0300

    task0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 const axios = require('axios');
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 axios.get(apiUrl)
   .then(response => {
@@ -12,13 +12,18 @@ axios.get(apiUrl)
     const characterPromises = characters.map(characterUrl =>
       axios.get(characterUrl)
         .then(charResponse => charResponse.data.name)
+        .catch(error => {
+          console.error(`Error fetching character: ${error.message}`);
+          return null;
+        })
     );
 
     return Promise.all(characterPromises);
   })
   .then(characterNames => {
-    characterNames.forEach(name => console.log(name));
+    const validCharacterNames = characterNames.filter(name => name !== null);
+    validCharacterNames.forEach(name => console.log(name));
   })
   .catch(error => {
-    console.error(error.message);
+    console.error(`Error fetching movie details: ${error.message}`);
   });
